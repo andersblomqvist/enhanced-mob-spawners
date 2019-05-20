e8f01a3998d938e47b9a4276522deae31dfa92d7
Anders Blomqvist <anders@minaemail.se>
2019-05-20 22:26:35 +0200

    Changed how we get entity id from spawner

--- java/com/branders/spawnermod/event/SpawnerEventHandler.java
+++ java/com/branders/spawnermod/event/SpawnerEventHandler.java
@@ -1,7 +1,5 @@
 package com.branders.spawnermod.event;
 
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
 import java.util.Random;
 
 import net.minecraft.block.state.IBlockState;
@@ -12,11 +10,11 @@ import net.minecraft.init.Blocks;
 import net.minecraft.item.ItemBlock;
 import net.minecraft.item.ItemSpawnEgg;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.nbt.NBTTagList;
 import net.minecraft.tileentity.MobSpawnerBaseLogic;
 import net.minecraft.tileentity.TileEntityMobSpawner;
 import net.minecraft.util.EnumHand;
-import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.minecraftforge.event.entity.living.LivingDropsEvent;
@@ -35,12 +33,11 @@ import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 @EventBusSubscriber
 public class SpawnerEventHandler
 {
-	private float SPAWN_RATE = 0.04F;
-	private Random random = new Random();
+    private float SPAWN_RATE = 0.04F;
+    private Random random = new Random();
+    private EntityType<?> defaultEntityType = EntityType.AREA_EFFECT_CLOUD;
 	
-	private EntityType<?> defaultEntityType = EntityType.AREA_EFFECT_CLOUD;
-	
-	/**
+    /**
      * 	When we harvest a block
      * 	Return spawner block when harvested with silk touch
      */
@@ -114,12 +111,12 @@ public class SpawnerEventHandler
     	Entity entity = event.getEntity();
     	EntityType<?> entityType = entity.getType();
     	
-		// Get the entity mob egg and put in an ItemStack
-		ItemSpawnEgg egg = ItemSpawnEgg.getEgg(entityType);
-		ItemStack itemStack = new ItemStack(egg);
-		
-		// Add egg in drops
-		event.getDrops().add(new EntityItem(entity.world, entity.posX, entity.posY, entity.posZ, itemStack));
+	// Get the entity mob egg and put in an ItemStack
+	ItemSpawnEgg egg = ItemSpawnEgg.getEgg(entityType);
+	ItemStack itemStack = new ItemStack(egg);
+
+	// Add egg in drops
+	event.getDrops().add(new EntityItem(entity.world, entity.posX, entity.posY, entity.posZ, itemStack));
     }
     
     
@@ -137,73 +134,61 @@ public class SpawnerEventHandler
     		return;
     	
     	BlockPos blockpos = event.getPos();
-		IBlockState iblockstate = world.getBlockState(blockpos);	
+	IBlockState iblockstate = world.getBlockState(blockpos);	
 		
-    	// Check if we right-clicked
-		if(world.getBlockState(blockpos).getBlock() == Blocks.SPAWNER && event.getHand() == EnumHand.MAIN_HAND)
-    	{	
-			// Return mob egg
-			try {
-				DropMonsterEgg(world, blockpos, iblockstate);
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-    	}
+    	// Check if we right-clicked and return mob egg from spawner
+	if(world.getBlockState(blockpos).getBlock() == Blocks.SPAWNER && event.getHand() == EnumHand.MAIN_HAND)
+		DropMonsterEgg(world, blockpos, iblockstate);
+			
     }
     
     /**
      * 	Spawns a mob egg depending on what type of entity inside mob spawner.
      * 	When successfully retrieved monster egg we set spawner entity to default.
-     * 
-     * 	Uses Java reflection to get entity type inside spawner
-	 * 
-     * 	@throws SecurityException 
-     * 	@throws NoSuchMethodException 
-     * 	@throws InvocationTargetException 
-     * 	@throws IllegalArgumentException 
-     * 	@throws IllegalAccessException
      */
-    private void DropMonsterEgg(World world, BlockPos blockpos, IBlockState iblockstate) throws NoSuchMethodException, SecurityException, IllegalAccessException, IllegalArgumentException, InvocationTargetException
+    private void DropMonsterEgg(World world, BlockPos blockpos, IBlockState iblockstate)
     {
     	TileEntityMobSpawner spawner = (TileEntityMobSpawner)world.getTileEntity(blockpos);
-    	
-    	if(spawner == null)
-    		System.out.println("Spawner is null");
-    	
     	MobSpawnerBaseLogic logic = spawner.getSpawnerBaseLogic();
-		    	
-		// Use Java Reflection to call private method getEntityId in MobSpawnerBaseLogic class
-		Method method = logic.getClass().getSuperclass().getDeclaredMethod("getEntityId");
-		method.setAccessible(true);
-		ResourceLocation resource = (ResourceLocation)method.invoke(logic);
+    	
+    	// Get entity ResourceLocation string from spawner by creating a empty NBTTagComp which we make our 
+    	// spawner logic write to. We can then access what type of entity id the spawner has inside
+    	NBTTagCompound nbtIn = new NBTTagCompound();
+    	nbtIn = logic.writeToNBT(nbtIn);
+    	String entity_string = nbtIn.getTag("SpawnData").getString();
+    	
+    	// Strips the string
+    	// Example: {id: "minecraft:xxx_xx"} --> minecraft:xxx_xx
+    	entity_string = entity_string.substring(entity_string.indexOf("\"") + 1);
+    	entity_string = entity_string.substring(0, entity_string.indexOf("\""));
+    	
+	// Get entity type
+	EntityType<?> entityType = EntityType.getById(entity_string);
 
-		// Get entity
-		EntityType<?> entityType = EntityType.getById(resource.toString());
-		
-		// Leave if the spawner does not contain an egg
-		if(entityType.equals(defaultEntityType))
-			return;
+	// Leave if the spawner does not contain an egg
+	if(entityType.equals(defaultEntityType))
+		return;
 		
     	// Get the entity mob egg and put in an ItemStack
-		ItemSpawnEgg egg = ItemSpawnEgg.getEgg(entityType);
-		ItemStack itemStack = new ItemStack(egg);
-		
-		// Get random fly-out position offsets
-		double d0 = (double)(world.rand.nextFloat() * 0.7F) + (double)0.15F;
+	ItemSpawnEgg egg = ItemSpawnEgg.getEgg(entityType);
+	ItemStack itemStack = new ItemStack(egg);
+
+	// Get random fly-out position offsets
+	double d0 = (double)(world.rand.nextFloat() * 0.7F) + (double)0.15F;
         double d1 = (double)(world.rand.nextFloat() * 0.7F) + (double)0.06F + 0.6D;
         double d2 = (double)(world.rand.nextFloat() * 0.7F) + (double)0.15F;
         
         // Create entity item
-		EntityItem entityItem = new EntityItem(world, (double)blockpos.getX() + d0, (double)blockpos.getY() + d1, (double)blockpos.getZ() + d2, itemStack);
-		entityItem.setDefaultPickupDelay();
-		
-		// Spawn entity item (egg)
-		world.spawnEntity(entityItem);
-		
-		// Replace the entity inside the spawner with default entity
-		logic.setEntityType(defaultEntityType);
-		spawner.markDirty();
-		world.notifyBlockUpdate(blockpos, iblockstate, iblockstate, 3);
+	EntityItem entityItem = new EntityItem(world, (double)blockpos.getX() + d0, (double)blockpos.getY() + d1, (double)blockpos.getZ() + d2, itemStack);
+	entityItem.setDefaultPickupDelay();
+
+	// Spawn entity item (egg)
+	world.spawnEntity(entityItem);
+
+	// Replace the entity inside the spawner with default entity
+	logic.setEntityType(defaultEntityType);
+	spawner.markDirty();
+	world.notifyBlockUpdate(blockpos, iblockstate, iblockstate, 3);
     }
     
     /**
@@ -216,9 +201,9 @@ public class SpawnerEventHandler
     private boolean CheckSilkTouch(NBTTagList list)
     {
     	// Check list string contains silk touch
-		if(list.getString().indexOf("minecraft:silk_touch") != -1)
-			return true;
-		else
-			return false;
+	if(list.getString().indexOf("minecraft:silk_touch") != -1)
+		return true;
+	else
+		return false;
     }
 }
