1c35dacad3e0044f1eacee0afbdca457dc9c1397
Anders Blomqvist <anders@minaemail.se>
2019-05-23 16:18:05 +0200

    Added Spawner to Creative Tabs

--- java/com/branders/spawnermod/SpawnerMod.java
+++ java/com/branders/spawnermod/SpawnerMod.java
@@ -2,8 +2,15 @@ package com.branders.spawnermod;
 
 import com.branders.spawnermod.event.SpawnerEventHandler;
 
+import net.minecraft.init.Blocks;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemBlock;
+import net.minecraft.item.ItemGroup;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.registries.IForgeRegistry;
 
 /**
  * 	Small mod adding more functionality to Mob Spawners
@@ -11,16 +18,33 @@ import net.minecraftforge.fml.common.Mod;
  * 	@author Branders
  *
  */
+@Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
 @Mod(SpawnerMod.modid)
 public class SpawnerMod
 {		
 	public static final String modid = "spawnermod";
 	
 	/**
-	* 	Register event handler
-	*/
+	 * 	Register event handler
+	 */
     public SpawnerMod() 
     {
     	MinecraftForge.EVENT_BUS.register(new SpawnerEventHandler());
+    }
+    
+    /**
+     * 	Event for register spawner item block
+     */
+    @SubscribeEvent
+    public static void registerItems(RegistryEvent.Register<Item> event) 
+    {
+        registerItems(event.getRegistry());
+    }
+    
+    public static void registerItems(IForgeRegistry<Item> registry)
+    {
+    	// Register Spawner ItemBlock as a "new" item block
+    	// It only makes the spawner show up in decoration tab
+    	registry.register(new ItemBlock(Blocks.SPAWNER, new Item.Properties().group(ItemGroup.DECORATIONS)).setRegistryName(modid, "spawner"));
     }
 }
