2eb198b37ee188fd10d59876a7575c05f76fc80a
Anders Blomqvist <anders@minaemail.se>
2022-07-06 12:25:10 +0200

    Spawner now remember that it has been set.

--- src/main/java/com/branders/spawnermod/mixin/SpawnerRangeMixin.java
+++ src/main/java/com/branders/spawnermod/mixin/SpawnerRangeMixin.java
@@ -3,6 +3,7 @@ package com.branders.spawnermod.mixin;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import com.branders.spawnermod.config.ConfigValues;
@@ -13,7 +14,7 @@ import net.minecraft.world.level.BaseSpawner;
 import net.minecraft.world.level.Level;
 
 /**
- *	
+ *	Sets the required player range one time to config value.
  * 
  * 	@author Anders <Branders> Blomqvist
  */
@@ -47,5 +48,23 @@ public class SpawnerRangeMixin {
         	
         	rangeSet = true;
 		}
+	}
+	
+	@Inject(at = @At(value = "INVOKE", target = "Lnet/minecraft/nbt/CompoundTag;getShort(Ljava/lang/String;)S"), method = "load")
+	private void load(Level level, BlockPos pos, CompoundTag nbt, CallbackInfo info) {
+
+		if(ConfigValues.get("default_spawner_range_enabled") == 0)
+			return;
+		
+		rangeSet = nbt.getBoolean("RangeSet");
+	}
+	
+	@Inject(at = @At(value = "INVOKE", target = "Lnet/minecraft/nbt/CompoundTag;putShort(Ljava/lang/String;S)V"), method = "save")
+	private void save(CompoundTag nbt, CallbackInfoReturnable<CompoundTag> info) {
+		
+		if(ConfigValues.get("default_spawner_range_enabled") == 0)
+			return;
+		
+		nbt.putBoolean("RangeSet", rangeSet);
 	}
 }
